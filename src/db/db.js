c9d7ea2ef42d4e9c565bc6aa1094e8613d94b3c6
Ashish Singh <ashishsingh92003@gmail.com>
2024-11-20 18:48:14 +0530

    DB connection

---
+++ src/db/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+// Connect to MongoDB
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n Connected to MongoDB !! DB Host ${connectionInstance.connection.host} \n`
+    );
+    // console.log(`MongoDB Connected: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("MONGODB is not connected", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import express from "express";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/db.js";
+
+const app = express();
+
+dotenv.config();
+
+connectDB();
+
+// this is the code to connect to the database using a iffi function form the javascript
+
+/*(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.error("Error connecting to the database");
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port  , ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR", error);
+    throw error.message;
+  }
+})();
+*/
